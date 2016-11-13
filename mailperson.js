08b8c6da4835036a7e03ffc1fa835fa3a5f78dfa
Lacroix Coconut <lacroix@coconut.codes>
2016-11-12 23:50:14 -0500

    Enable passing app in config

--- mailperson.js
+++ mailperson.js
@@ -3,12 +3,13 @@
 module.exports = function(config){
 
     const Koa = require('koa');
+    const app = config.app || new Koa();
+
     const EmailService = require('./lib/email_service')(config.email_service);
     const Renderer = require('./lib/renderer');
     const renderer = new Renderer(config.renderer);
     const Router = require('./lib/router')(EmailService, renderer);
     const router = new Router(config.routes);
-    const app = new Koa();
 
     if(config.middleware){
         config.middleware.forEach(middleware => { app.use(middleware) } );
